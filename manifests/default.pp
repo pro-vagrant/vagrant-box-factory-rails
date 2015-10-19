cb335afe299f1e1aa6ef8c524dc0a44c1acffe79
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-10-19 17:47:42 +0200

    Fix syntax

--- manifests/default.pp
+++ manifests/default.pp
@@ -13,6 +13,6 @@ class { nodejs: }
 
 class { 'bundle_install':
     repo    => 'https://github.com/pro-vagrant/songs-app-rails.git',
-    command => 'bundle update'
+    command => 'bundle update',
     require => Class['ruby', 'rails', 'nodejs']
 }

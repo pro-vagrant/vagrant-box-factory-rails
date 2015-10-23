423344b0c63a30aa374648baae4af82557457b3c
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-10-23 19:44:16 +0200

    Use latest version of environment module

--- install-puppet-modules.sh
+++ install-puppet-modules.sh
@@ -11,7 +11,7 @@ sudo puppet module install puppetlabs-stdlib --version 4.6.0 --force
 sudo puppet module install gajdaw-ruby --version 0.5.3 --force
 sudo puppet module install gajdaw-diverse_functions --version 0.1.1 --force
 sudo puppet module install gajdaw-rails --version 0.2.0 --force
-sudo puppet module install gajdaw-environment --version 0.1.4 --force
+sudo puppet module install gajdaw-environment --version 0.5.0 --force
 sudo puppet module install gajdaw-ubuntu --version 0.1.13 --force
 sudo puppet module install gajdaw-nodejs --version 0.2.2 --force
 sudo puppet module install gajdaw-bundle_install --version 0.1.1 --force

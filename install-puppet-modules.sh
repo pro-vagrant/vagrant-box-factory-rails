f885f397978d4990f21bb7b662e8a04397a3525c
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-10-28 18:25:49 +0100

    Remove unnecessary modules form install script

--- install-puppet-modules.sh
+++ install-puppet-modules.sh
@@ -7,19 +7,12 @@ echo "Install Puppet modules..."
 #     sudo puppet module install gajdaw-rails
 #
 
-sudo puppet module install puppetlabs-stdlib
 sudo puppet module install puppetlabs-apt
-sudo puppet module install puppetlabs-apache
-sudo puppet module install puppetlabs-mysql
 sudo puppet module install maestrodev-wget
 sudo puppet module install gajdaw-ruby
-sudo puppet module install gajdaw-diverse_functions
 sudo puppet module install gajdaw-rails
 sudo puppet module install gajdaw-environment
-sudo puppet module install gajdaw-ubuntu
 sudo puppet module install gajdaw-nodejs
 sudo puppet module install gajdaw-bundle_install
-sudo puppet module install gajdaw-vcsrepository
-sudo puppet module install gajdaw-php5
 sudo puppet module install gajdaw-phpmyadmin
 
